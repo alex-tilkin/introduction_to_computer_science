d47df76a065dd1600d60c96f747f4b2e74559033
Alex Tilkin <alextilk@gmail.com>
2018-12-08 14:38:51 +0200

    new drills were added

---
+++ src/unit_3/WhileLoopExample.java
@@ -0,0 +1,16 @@
+package unit_3;
+
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int count = 0;
+		int STOP_CONDITION = 3;
+
+		while(count < STOP_CONDITION){
+		sum += count;
+		count++;
+		System.out.printf("The values of sum and count are %d and %d\n", sum, count);
+		}
+	}
+}
