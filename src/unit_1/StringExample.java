c3c2c1a8ce4246e2a0feef184eb286310e291396
Alexander Tilkin <alextilk@gmail.com>
2019-02-28 20:23:03 +0200

    New examples for unit 1

---
+++ src/unit_1/StringExample.java
@@ -0,0 +1,9 @@
+package unit_1;
+
+public class StringExample {
+	public static void main(String[] args) {
+		String myString;
+		myString = "This is my first string";
+		System.out.println(myString);
+	}
+}
