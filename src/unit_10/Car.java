e82e7731b9b7ef07e83111183df18167159b0b76
Alex Tilkin <alextilk@gmail.com>
2019-04-16 22:01:49 +0300

    New files for unit 10

---
+++ src/unit_10/Car.java
@@ -0,0 +1,9 @@
+package unit_10;
+
+public class Car {
+	private SteeringWheel steeringWheel;
+	
+	public Car(SteeringWheel steeringWheel) {
+		this.steeringWheel = new SteeringWheel(steeringWheel);
+	}
+}
