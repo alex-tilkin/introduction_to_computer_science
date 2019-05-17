9e6d872a95b7dbe86ec44b404b0371ade88811fe
Alex Tilkin <alextilk@gmail.com>
2019-05-17 10:45:29 +0300

    Adding new files

---
+++ src/unit_12/SimpleAdditionalFileMethods.java
@@ -0,0 +1,35 @@
+package unit_12;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SimpleAdditionalFileMethods {
+
+	public static void main(String[] args) throws IOException {
+		File[] files = new File[3];
+		
+		files[0] = new File("src/unit_12/storage/SimpleFile.txt");
+		files[1] = new File("src/unit_12/storage/");
+		files[2] = new File("src/unit_12/storage/additional_file.txt");
+		
+		File[] all = files[1].listFiles();
+		for (File file : all) {
+			System.out.println(file.getAbsolutePath());
+		}
+		
+		System.out.println();
+		files[2].createNewFile();
+		all = files[1].listFiles();
+		for (File file : all) {
+			System.out.println(file.getAbsolutePath());
+		}
+		
+		System.out.println();
+		
+		files[2].delete();
+		all = files[1].listFiles();
+		for (File file : all) {
+			System.out.println(file.getAbsolutePath());
+		}
+	}
+}
