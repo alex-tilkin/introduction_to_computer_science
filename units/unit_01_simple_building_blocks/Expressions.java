d7d18e334e4451dd2316f2910c6b016bdb05313f
Alexander Tilkin <alextilk@gmail.com>
2020-02-29 15:25:54 +0200

    Modifications

--- units/unit_01_simple_building_blocks/Expressions.java
+++ units/unit_01_simple_building_blocks/Expressions.java
@@ -4,12 +4,8 @@ public class Expressions {
 
 	public static void main(String[] args) {
 		int cadence = 0; 
-		System.out.println("The value of cadence is: " + cadence);
 		int result = 1 + 2;
 		int value1 = 1;
 		int value2 = 1;
-		if (value1 == value2) {
-			System.out.println("value1 == value2");
-		}
 	}
 }
--- units/unit_01_simple_building_blocks/Statements.java
+++ units/unit_01_simple_building_blocks/Statements.java
@@ -2,15 +2,19 @@ package unit_01_simple_building_blocks;
 
 public class Statements {
 	public static void main(String[] args) {
-		// this is an expression
-		int aValue;
-		// assignment statement
-		aValue = 8933;
-		// increment statement
-		aValue++;
-		// method invocation statement
-		System.out.println("Hello World!");
-		// object creation statement
-		//Bicycle myBike = new Bicycle();
+			// this is an expression
+			int aValue;
+			// assignment statement
+			aValue = 8933;
+			// increment statement
+			aValue++;
+			// method invocation statement
+			System.out.println("Hello World!");
+			// object creation statement
+			//Bicycle myBike = new Bicycle();
+			
+			if(aValue > 0) {
+				System.out.println("this is a control-flow statement");
+			}
 		}
 }
